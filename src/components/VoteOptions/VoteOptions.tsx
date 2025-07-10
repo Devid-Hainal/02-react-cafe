bdc9113e8f1483b23ec82142322f0a0d265b8f12
Devid Hainal <devidhainal8@gmail.com>
2025-07-10 23:17:30 +0300

    adding tsx markup

---
+++ src/components/VoteOptions/VoteOptions.tsx
@@ -0,0 +1,32 @@
+import type { VotesType } from "../../types/votes";
+import css from "./VoteOptions.module.css";
+
+interface VotesOptionsProps {
+  onVote: (type: VotesType) => void;
+  onReset: () => void;
+  canReset: boolean;
+}
+export default function VoteOptions({
+  onVote,
+  onReset,
+  canReset,
+}: VotesOptionsProps) {
+  return (
+    <div className={css.container}>
+      <button className={css.button} onClick={() => onVote("good")}>
+        Good
+      </button>
+      <button className={css.button} onClick={() => onVote("neutral")}>
+        Neutral
+      </button>
+      <button className={css.button} onClick={() => onVote("bad")}>
+        Bad
+      </button>
+      {canReset && (
+        <button className={`${css.button} ${css.reset}`} onClick={onReset}>
+          Reset
+        </button>
+      )}
+    </div>
+  );
+}
